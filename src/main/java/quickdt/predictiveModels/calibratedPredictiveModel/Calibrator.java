c7d7f92b7aa2e047e4c8d025cd33fe4ab62159cc
ed <ed.developer90@gmail.com>
2021-02-05 18:35:48 +0330

    first initial.

---
+++ src/main/java/quickdt/predictiveModels/calibratedPredictiveModel/Calibrator.java
@@ -0,0 +1,8 @@
+package quickdt.predictiveModels.calibratedPredictiveModel;
+
+/**
+ * Created by alexanderhawk on 3/11/14.
+ */
+public interface Calibrator {
+    public double correct(final double input);
+}
